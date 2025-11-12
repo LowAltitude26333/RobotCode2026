f6839e3334f456f01ebd4d20c38d31b44ca41c4f
joaquinrog <charinoxc@gmail.com>
2025-11-12 17:16:15 -0600

    Base 2026: SDK v11.0 + FTCLib configurado

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCLibTestOpMode.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "FTCLib Test", group = "Test")
+public class FTCLibTestOpMode extends com.arcrobotics.ftclib.command.CommandOpMode {
+
+    private Motor leftMotor;
+
+    @Override
+    public void initialize() {
+        leftMotor = new Motor(hardwareMap, "left_drive");
+    }
+
+    @Override
+    public void run() {
+        double power = -gamepad1.left_stick_y;
+        leftMotor.set(power);
+    }
+}
