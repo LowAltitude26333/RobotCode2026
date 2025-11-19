a661ec5cc9b7dc3167c9fdd8ae39d2983836ce58
Ceryle1 <leonardo.martinez.raz4@gmail.com>
2025-11-18 19:00:48 -0600

    feat: Shooter Subsystem Sketch :3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LowAltitudeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LowAltitudeConstants.java
@@ -3,4 +3,25 @@ package org.firstinspires.ftc.teamcode;
 public class LowAltitudeConstants {
 
     public static final double INTAKE_IN_SPEED = 0.0;
+
+    //Shooter
+    public static final double SHOOTER_ON_SPEED = 0.5;
+    public static final double SHOOTER_OFF_SPEED = 0.0;
+    public static final double SHOOTER_INTAKE_SPEED = -0.5;
+
+    //PID Shooter
+    public static final double SHOOTER_KP = 0.37;
+    public static final double SHOOTER_KI = 0.05;
+    public static final double SHOOTER_KD = 1.02;
+    public static final double SHOOTER_KF = 0.7;
+
+    //Tolerance
+    public static final double RPM_OFFSET = 50;
+    public static final double TICKS_PER_REV = 28.0;
+    public static final double GEAR_RATIO = 2;
+
+
+    //Feedforward Shooter
+    public static final double SHOOTER_KS = 0.37;
+    public static final double SHOOTER_KV = 0.05;
 }
